955ecc17063bc8306e9e4570cccbf6ba60ef512c
dusanov <sadriddindusanov@gmail.com>
2026-05-07 13:00:00 +0500

    Fixed

--- src/main/java/com/course/controller/CourseInstructorController.java
+++ src/main/java/com/course/controller/CourseInstructorController.java
@@ -4,10 +4,7 @@ import com.course.dto.CourseInstructorDTO;
 import com.course.service.CourseInstructorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("course-instructor")
@@ -15,7 +12,7 @@ public class CourseInstructorController {
     @Autowired
     private CourseInstructorService service;
 
-    @GetMapping("/create")
+    @PostMapping("/create")
     public ResponseEntity<CourseInstructorDTO> save(@RequestBody CourseInstructorDTO dto){
         return ResponseEntity.ok(service.save(dto));
     }
